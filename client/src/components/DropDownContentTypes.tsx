f9364ab1f2726069683497fb964bd076cdfa4c40
ahmediiqbal <56063651+ahmediiqbal@users.noreply.github.com>
2022-01-26 13:57:14 +0100

    Update DropDownContentTypes.tsx
    "ADD" Button and Row style

--- client/src/components/DropDownContentTypes.tsx
+++ client/src/components/DropDownContentTypes.tsx
@@ -1,17 +1,22 @@
-import { Form, Col, Row} from 'react-bootstrap';
+import { Form, Col, Row, Button} from 'react-bootstrap';
 
 const DropDownContentTypes = (props:any) => {
     
 return (
-    <>
-    <Form.Select>
+    <Row style = {{marginTop:"2rem"}}>
+        <Col xs md lg={{span: 4,offset: 5}} style={{marginLeft:"39rem"}} >
+    <Form.Select style = {{border:"solid black 3px"}}>
         <option>Choose contentTypes</option>
         {props.contenttype.map((element:string)=>(
             <option>{element}</option>
         ) )}
-    </Form.Select>
 
-    </>
+    </Form.Select>
+    </Col>
+    <Col>
+    <Button variant="primary">ADD</Button>
+    </Col>
+    </Row>
 )
 };
 
