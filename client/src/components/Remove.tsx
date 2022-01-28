fbc8d13b2dca873a8d7dccedad4940165590f92c
Guled1212 <guled01212@hotmail.com>
2022-01-28 16:25:29 +0100

    Remove function, but its still under construction

--- client/src/components/Remove.tsx
+++ client/src/components/Remove.tsx
@@ -1,9 +1,15 @@
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const Remove = () => {
     return (
        <Row>
-
-           
+         <Col className="col-4">
+                    <img alt='remove field'   src="/images/remove.png" className="img" style={{width:"2rem"}}/>    
+                </Col>
+                <Col className="col-8 justify-content-center align-item-center">
+                    <p className="p">Move</p>
+                </Col>
        </Row>
     )
 }
