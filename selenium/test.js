98300ba6115d0d85a0497c3ce130dd881af7588a
KhalidAFarah <56063705+KhalidAFarah@users.noreply.github.com>
2022-05-02 19:30:20 +0200

    Set up a test that deletes a selected contenttype also added name fields to connect to the add, edit and delete button

--- selenium/test.js
+++ selenium/test.js
@@ -1,28 +1,26 @@
 const {Builder, By, Key, util} = require("selenium-webdriver");
-require("chromedriver");
+const assert = require("assert");
 
-async function example (){
-    //try{
-        /*let driver = await new Builder().forBrowser("chrome").build().catch((error) => {
-            console.log(error)
-
-            console.log("-----")
-        })*/
-
-        let driver = await new Builder().forBrowser('firefox').build()
-
-        await driver.get("http://google.com").catch((error) => {
-            
-        });
-        await driver.findElement(By.name("q")).sendKeys("Selenium Test", Key.RETURN).catch((error) => {
-            
-        });
-
-        //await driver.quit()
-    //}catch(error){
-    //    console.log(error)
-    //}
+async function deleteContenttype(contenttype){
+    // Starting the driver
+    let driver = await new Builder().forBrowser('firefox').build()
+    await driver.get("http://localhost:3000/")
     
+    // Selecting the contenttype
+    let select = await driver.findElement(By.id("ManageContentTypes_select"));
+    select.findElement(By.css("option[value='"+contenttype+"']")).click();
+    let data = await select.getText();
 
+    // Deleting the contenttype
+    await driver.findElement(By.name("delete_contenttype")).click();
+
+    // Checking that its deleted
+    let newData = await select.getText();
+    assert.notStrictEqual(newData, data);
+    let isEmpty = await select.getAttribute("value");
+    assert.strictEqual("", isEmpty);
+
+    // Closing window
+    await driver.quit();
 }
-example();
+deleteContenttype("brtikkel");
