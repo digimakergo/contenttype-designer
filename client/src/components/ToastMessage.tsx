bf4f32be3f84d388f117373e97c8fb11fd1f7896
ahmediiqbal <56063651+ahmediiqbal@users.noreply.github.com>
2022-05-04 14:44:30 +0200

    Update ToastMessage.tsx
    Color Designed

--- client/src/components/ToastMessage.tsx
+++ client/src/components/ToastMessage.tsx
@@ -2,8 +2,8 @@ import Toast from 'react-bootstrap/Toast';
 
 const ToastMessage = (props:any) => {
   return (
-    <Toast onClose={() => {props.setShow(false)}} show={props.show} delay={props.delay} style={{position:"fixed"}} autohide>
-        <Toast.Body>
+    <Toast onClose={() => {props.setShow(false)}} show={props.show} delay={props.delay} style={{position:"fixed",backgroundColor:"#198754"}} autohide>
+        <Toast.Body style={{backgroundColor: "#198754",color:"#FFFFFF", fontFamily:"sans-serif", fontSize:"1rem"}}>
             {props.text}
         </Toast.Body>
 
